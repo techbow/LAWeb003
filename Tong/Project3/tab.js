45124e76c139fdcaa59ceb26fb048446abf15d9e
Tong <tji132006@gmail.com>
2015-10-01 15:48:28 -0700

    Project3

---
+++ Tong/Project3/tab.js
@@ -0,0 +1,11 @@
+function changeTabA(){
+	document.getElementById("tabContent").innerHTML = "A";
+}
+
+function changeTabB(){
+	document.getElementById("tabContent").innerHTML = "B";
+}
+
+function changeTabC(){
+	document.getElementById("tabContent").innerHTML = "C";
+}
